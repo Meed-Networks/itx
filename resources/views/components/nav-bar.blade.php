1559900873a2da32c805f1a1a0432bf9e8be3e3a
Abdullahi <abdullahij951@gmail.com>
2021-06-18 08:05:21 +0100

    chore: extracted main nav bar into component

---
+++ resources/views/components/nav-bar.blade.php
@@ -0,0 +1,81 @@
+<div class="fixed top-0 w-full" id="navi">
+    <div class="flex items-center justify-between px-3 py-2 text-white">
+        <div class="text-xl font-bold text-purple-800">
+            LOGO
+        </div>
+        <div class="static" x-data="dropdown()">
+            <button class="text-purple-800 hover:outline-none lg:hidden" x-on:click="open">
+                <svg class="w-6 h-6" :class="{'hidden': isOpen() == true }" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16">
+                    </path>
+                </svg>
+                <svg class="w-6 h-6" :class="{'hidden': isOpen() == false }" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12">
+                    </path>
+                </svg>
+
+            </button>
+            <ul class="box-content fixed right-0 grid justify-center grid-cols-1 mr-3 border border-b divide-y divide-grey-500 lg:flex lg:divide-none lg:bg-transparent lg:space-x-2 lg:relative divide-opacity-50 lg:border-none"
+                :class="{'hidden': isOpen() == false }" x-on:click.away="close">
+                <li>
+                    <x-nav-items link="#" linkName="Home" />
+                </li>
+                <li>
+                    <x-nav-items link="#" linkName="Services" />
+                </li>
+                <li>
+                    <x-nav-items link="#" linkName="pricing" />
+                </li>
+                <li>
+                    <x-nav-items link="#" linkName="Coverage" />
+                </li>
+                <li>
+                    <x-nav-items link="#" linkName="Contact Us" />
+                </li>
+            </ul>
+        </div>
+    </div>
+    <hr class="border-purple-900" width="0%" id="animeHr">
+</div>
+
+<script>
+    function dropdown() {
+        return {
+            show: false,
+            open() {
+                this.show = !this.show;
+            },
+            close() {
+                this.show = false;
+            },
+            isOpen() {
+                return this.show;
+            }
+        }
+    }
+
+    window.onscroll = function(e) {
+        var navi = document.getElementById("navi")
+        navi.classList.add("bg-gray-100")
+        navi.classList.add("bg-opacity-50")
+        if (window.scrollY == 0) {
+            navi.classList.remove("bg-gray-100")
+            navi.classList.remove("bg-opacity-50")
+        }
+
+
+        let scrollHeight = Math.max(
+            document.body.scrollHeight, document.documentElement.scrollHeight,
+            document.body.offsetHeight, document.documentElement.offsetHeight,
+            document.body.clientHeight, document.documentElement.clientHeight
+        ) - 667;
+
+        scrollOffset = window.pageYOffset;
+
+        var animeHr = document.getElementById('animeHr');
+        animeHr.style.width = ((scrollOffset / scrollHeight) * 100) + "%";
+    }
+
+</script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,41 +19,7 @@
 </head>
 
 <body class="bg-gray-50">
-    <div class="fixed top-0 w-full" id="navi">
-        <div class="flex items-center justify-between px-3 py-2 text-white">
-            <div class="text-xl font-bold text-purple-800">
-                LOGO
-            </div>
-            <div class="static" x-data="dropdown()">
-                <button class="text-purple-800 hover:outline-none lg:hidden" x-on:click="open">
-                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M4 6h16M4 12h16M4 18h16"></path>
-                    </svg>
-                </button>
-                <ul class="fixed right-0 grid items-center justify-center grid-cols-1 mr-3 border divide-y divide-grey-500 lg:flex lg:divide-none lg:bg-transparent lg:space-x-2 lg:relative divide-opacity-50"
-                    :class="{'hidden': isOpen() == false }" x-on:click.away="close">
-                    <li>
-                        <x-nav-items link="#" linkName="Home"/>
-                    </li>
-                    <li>
-                        <x-nav-items link="#" linkName="Services"/>
-                    </li>
-                    <li>
-                        <x-nav-items link="#" linkName="pricing"/>
-                    </li>
-                    <li>
-                        <x-nav-items link="#" linkName="Coverage"/>
-                    </li>
-                    <li>
-                        <x-nav-items link="#" linkName="Contact Us"/>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <hr class="border-purple-900" width="0%" id="animeHr">
-    </div>
+    <x-nav-bar />
     <div style="background-image: url('{{ asset('img/img3.png') }}')"
         class="static flex flex-col items-center justify-center h-screen m-0 bg-fixed bg-center bg-no-repeat bg-cover"
         id="slider">
@@ -251,44 +217,6 @@
                 i = 0;
             }
         }, 5000);
-
-        window.onscroll = function(e) {
-            var navi = document.getElementById("navi")
-            navi.classList.add("bg-gray-100")
-            navi.classList.add("bg-opacity-50")
-            if (window.scrollY == 0) {
-                navi.classList.remove("bg-gray-100")
-                navi.classList.remove("bg-opacity-50")
-            }
-
-
-            let scrollHeight = Math.max(
-                document.body.scrollHeight, document.documentElement.scrollHeight,
-                document.body.offsetHeight, document.documentElement.offsetHeight,
-                document.body.clientHeight, document.documentElement.clientHeight
-            )-667;
-
-            scrollOffset = window.pageYOffset;
-
-            var animeHr = document.getElementById('animeHr');
-            animeHr.style.width = ((scrollOffset/scrollHeight) * 100) + "%";
-        }
-
-        function dropdown() {
-            return {
-                show: false,
-                open() {
-                    this.show = !this.show;
-                },
-                close() {
-                    this.show = false;
-                },
-                isOpen() {
-                    return this.show;
-                }
-            }
-        }
-
     </script>
 </body>
 
