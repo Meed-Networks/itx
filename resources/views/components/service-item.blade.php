25f2dc313512959dcdae9d9b4a91cd1175b83d7e
Abdullahi <abdullahij951@gmail.com>
2021-06-18 08:12:00 +0100

    extracted service into componnet

---
+++ resources/views/components/service-item.blade.php
@@ -0,0 +1,53 @@
+<div class="flex flex-wrap items-end -mt-20 space-y-5 lg:-mt-64 justify-evenly lg:h-96">
+    <div
+        class="flex flex-col items-center justify-center px-5 py-10 space-y-3 bg-white shadow-md lg:hover:-mt-24 w-60 group">
+        <div class="w-20 h-20 bg-center bg-no-repeat bg-cover hover:animate-bounce"
+            style="background-image: url('{{ asset('img/icons/rocket.svg') }}')">
+
+        </div>
+        <div class="flex flex-col items-center justify-center h-20 font-semibold sm:hidden group-hover:flex">
+            <p class="text-center ">
+                High Speed Internet connection
+            </p>
+        </div>
+    </div>
+
+    <div
+        class="flex flex-col items-center justify-center px-5 py-10 space-y-3 bg-white shadow-md lg:hover:-mt-24 w-60 group">
+        <div class="w-20 h-20 bg-center bg-no-repeat bg-cover hover:animate-bounce"
+            style="background-image: url('{{ asset('img/icons/bandwidth.svg') }}')">
+
+        </div>
+        <div class="flex flex-col items-center justify-center h-20 font-semibold sm:hidden group-hover:flex">
+            <p class="text-center ">
+                Unlimited Data
+            </p>
+        </div>
+    </div>
+
+    <div
+        class="flex flex-col items-center justify-center px-5 py-10 space-y-3 bg-white shadow-md lg:hover:-mt-24 w-60 group">
+        <div class="w-20 h-20 bg-center bg-no-repeat bg-cover hover:animate-bounce"
+            style="background-image: url('{{ asset('img/icons/server.svg') }}')">
+
+        </div>
+        <div class="flex flex-col items-center justify-center h-20 font-semibold sm:hidden group-hover:flex">
+            <p class="text-center ">
+                Free Installation
+            </p>
+        </div>
+    </div>
+
+    <div
+        class="flex flex-col items-center justify-center px-5 py-10 space-y-3 bg-white shadow-md lg:hover:-mt-24 w-60 group">
+        <div class="w-20 h-20 bg-center bg-no-repeat bg-cover hover:animate-bounce"
+            style="background-image: url('{{ asset('img/icons/data-flow.svg') }}')">
+
+        </div>
+        <div class="flex flex-col items-center justify-center h-20 font-semibold sm:hidden group-hover:flex">
+            <p class="text-center ">
+                No Throttling
+            </p>
+        </div>
+    </div>
+</div>
