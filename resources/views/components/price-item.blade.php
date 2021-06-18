40b19ce2057ce46d13e9507aad0f013c171dd2da
Abdullahi <abdullahij951@gmail.com>
2021-06-18 13:39:10 +0100

    chore: created the price componnet

---
+++ resources/views/components/price-item.blade.php
@@ -0,0 +1,31 @@
+<div class="flex flex-col items-center justify-center p-5 space-y-3 transition duration-300 ease-in-out delay-150 border divide-y divide-purple-700 rounded min-w-max divide-opacity-50 hover:shadow-lg">
+    <!-- Order your soul. Reduce your wants. - Augustine -->
+    <div class="flex items-center justify-center">
+        <span class="text-lg font-bold text-purple-900">Simple</span>
+    </div>
+    <div class="flex flex-col items-center justify-center pt-3 space-y-2">
+        <div class="flex items-center justify-center space-x-2 font-semibold text-purple-900 group">
+            <span>
+                <svg class="w-6 h-6 " fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M7 16V4m0 0L3 8m4-4l4 4m6 0v12m0 0l4-4m-4 4l-4-4"></path>
+                </svg>
+            </span>
+            <span>1Mbps</span>
+        </div>
+        <div class="flex items-center justify-center space-x-2 font-semibold text-purple-900 group">
+            <span><svg class="w-6 h-6 " fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15">
+                    </path>
+                </svg></span>
+            <span>30days</span>
+        </div>
+        <div class="flex items-center justify-center text-white group">
+            <span class="px-2 py-2 font-bold bg-purple-700 rounded group-hover:hidden">₦ 20,000</span>
+            <button class="hidden px-2 py-2 font-bold bg-purple-700 rounded group-hover:inline-block">Subscribe</button>
+        </div>
+    </div>
+</div>
--- resources/views/pricing.blade.php
+++ resources/views/pricing.blade.php
@@ -1,5 +1,17 @@
 @extends('layout.main')
 
 @section('main')
-
+    <div class="flex flex-col items-center justify-center p-5 mt-10 space-y-5">
+        <div class="flex flex-col items-center justify-center space-y-2 text-purple-900">
+            <span class="text-xl font-bold ">
+                Simple, transparent pricing
+            </span>
+            <span class="text-sm font-semibold text-purple-500 text-opacity-50">
+                No contracts, No suprise fees.
+            </span>
+        </div>
+        <div class="flex flex-wrap items-center justify-center">
+            <x-price-item />
+        </div>
+    </div>
 @endsection
