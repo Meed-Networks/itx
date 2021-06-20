40da004ab7d8455593a495ac48ada91366d4af93
Abdullahi <abdullahij951@gmail.com>
2021-06-20 08:56:54 +0100

    chore: created tab for pricing packages

--- resources/views/pricing.blade.php
+++ resources/views/pricing.blade.php
@@ -1,17 +1,27 @@
 @extends('layout.main')
 
 @section('main')
-    <div class="flex flex-col items-center justify-center p-5 mt-10 space-y-5">
-        <div class="flex flex-col items-center justify-center space-y-2 text-purple-900">
-            <span class="text-xl font-bold ">
-                Simple, transparent pricing
-            </span>
-            <span class="text-sm font-semibold text-purple-500 text-opacity-50">
-                No contracts, No suprise fees.
+    <div class="px-5 mt-10 py-5" x-data="{tab: 1}">
+        <div
+            class="flex items-center justify-center flex-col space-y-5 py-5 md:flex-row md:justify-between md:px-10 lg:px-20">
+            <div class="flex items-center flex-col justify-center lg:items-start">
+                <span class="font-extrabold text-sm md:text-xl text-purple-900">Simple, transparent pricing</span>
+                <span class="font-semibold text-xs opacity-50 text-gray-400">No contracts, No suprise fees</span>
+            </div>
+            <span class="flex border rounded-full text-gray-700 bg-clip-border shadow-sm">
+                <button x-on:click="tab = 1" :class="{'bg-purple-700 text-white ' : tab == 1 }"
+                    class="px-3 py-2 text-xs font-semibold  rounded-full focus:outline-none">Residential</button>
+                <button x-on:click="tab = 2" :class="{'bg-purple-700 text-white ' : tab == 2 }"
+                    class="px-3 py-2 text-xs font-semibold  rounded-full focus:outline-none">Corporate</button>
             </span>
         </div>
-        <div class="flex flex-wrap items-center justify-center">
-            <x-price-item />
+        <div>
+            <div class="flex flex-wrap items-center justify-center w-full" :class="{'hidden': tab != 1}">
+                <x-price-item />
+            </div>
+            <div class="flex" :class="{'hidden': tab != 2}">
+                Hello
+            </div>
         </div>
     </div>
 @endsection
